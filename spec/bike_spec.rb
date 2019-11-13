1b74915cb97a8e010a71277733b9ba96f1b68223
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-13 15:56:47 +0000

    Adding notes to spec files

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,6 +7,7 @@ describe Bike do
 #Test: To see if our Bike instance responds to method working?
     it {is_expected.to respond_to :working?}
 
+# Test: Return true if the bike is working.
     it "Should return true if the bike is working" do 
         expect(bike.working?).to be_truthy
     end
