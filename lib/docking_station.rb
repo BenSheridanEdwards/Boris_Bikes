c76ec65097f6f3164d0695bceb3a852397e13f0e
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-13 17:23:58 +0000

    Adding raise and dock_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,29 +2,40 @@ require_relative 'bike'
 
 class DockingStation
   
-  def initialize
+  def initialize(capacity = 20)
     @dock = []
-    @capacity = 20
+    @capacity = capacity
   end
 
   def release_bike
     
-    if dock.size == 0 
-        return "Dock empty"
-    elsif dock.size <= 1
+    if dock.size <= 1
         bike = dock.pop
         return bike if bike.working?
+    elsif
+        @dock.size == 0 
+        return "Dock empty"
     end 
 
   end
 
   def dock_bike(bike, working = true)
-   
-    if dock.size == capacity
-      return "Can't return, dock full"
-    else 
-      dock << bike
-    end
+    raise "Full Dock, please come back later" if full? #Create test
+    raise "Your bike is broken! Take it to the garage" unless bike.working? #Create test
+    @dock << bike
+  end
 
+  end
+
+  private
+
+  attr_reader :dock
+
+  def full?
+    dock.count >= capacity
+  end
+
+  def empty?
+    dock.empty?
   end
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,10 @@ require 'bike'
 
 describe DockingStation do 
 
-    let(:bike) { Bike.new }
+  let(:bike) { Bike.new }
+
+
+    
 
     #Release a bike
     it { is_expected.to respond_to :release_bike }
@@ -11,4 +14,26 @@ describe DockingStation do
     # Check bike can be docked successfully
     it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
+
+    # Check our bike is passed into the dock
+    it "Stores a bike in the dock" do 
+        allow(bike).to receive(:working?)
+        expect(subject.dock_bike(bike)).to eq [bike]
+    end
+
+  describe '#dock_bike' do 
+
+   it "Raises an error when the dock is full" do
+    expect {full? == true}.to raise_error("Full Dock, please come back later")
+   end 
+
+   it "Raises an error when all the bikes are broken" do 
+    expect {bike.working? == false}.to raise_error("Your bike is broken! Take it to the garage.")
+   end
+
+  describe '#release_bike' do 
+
+  it "Raises an error when the dock is empty"
+
 end
+
