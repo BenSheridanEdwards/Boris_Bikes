cc497e136e5ba5002090eb392454139e52f9c166
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-04 13:09:58 +0000

    Rewriting bike spec with new knowledge of TDD using context blocks and doubles correctly

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,32 +1,31 @@
 require 'bike'
 
-describe Bike do
+RSpec.describe 'Bike' do
 
   let(:bike) { Bike.new }
 
-  describe '#working?' do
-
-    # Give feedback on whether a bike is working
-    it 'should return true if the bike is working' do
-      expect(bike.working?).to be_truthy
+  context 'when a bike is working' do
+    it "should return true when working? is called" do
+      expect(bike.working?).to eq true
     end
 
+    it 'should be able to be reported as broken, and return false when working? is called' do
+      bike.report_broken
+      expect(bike.working?).to eq false
+    end
   end
 
-  context 'when a bike has been reported as broken' do
-     
-    # Reporting a bike as broken should change calling .broken? from false to true
-    it 'should return true when the bike has been reported as broken' do
-      expect(bike.broken?).to be_falsey
+  context 'when a bike is broken' do
+    before(:each) do
       bike.report_broken
-      expect(bike.broken?).to be_truthy
     end
 
-    # Reporting a bike as broken should change calling .working? from true to false
-    it 'should change the attribute of a bike from working to not working' do
-      expect(bike.working?).to be_truthy
-      bike.report_broken
-      expect(bike.working?).to be_falsey
+    it 'should return true when broken? is called' do
+      expect(bike.broken?).to eq true
+    end
+
+    it 'should return false when working? is called' do
+      expect(bike.working?).to eq false
     end
   end
 end
